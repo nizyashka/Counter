2d0168052484898b191614e415a9552ae0b8177e
AlexeyNepryakhin <alekseynepryakhin@Aleksejs-MacBook-Air.local>
2024-12-09 21:23:52 +0300

    Проект выполнен

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var historyTextView: UITextView!
+    
+    var counter: Int = 0
+    
+    let dateFormatter = DateFormatter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "\(counter)"
+        historyTextView.layer.borderWidth = 2
+        historyTextView.layer.borderColor = UIColor.systemBlue.cgColor
+        historyTextView.layer.cornerRadius = 10
+    }
+    
+    func getDate(_ currentDate: Date) -> String {
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormatter.string(from: currentDate)
     }
 
-
+    @IBAction func plusOne() {
+        counter += 1
+        counterLabel.text = "\(counter)"
+        historyTextView.text += "\n[\(getDate(Date.now))]: Значение изменено на +1"
+    }
+    
+    @IBAction func minusOne() {
+        if counter > 0 {
+            counter -= 1
+            counterLabel.text = "\(counter)"
+            historyTextView.text += "\n[\(getDate(Date.now))]: Значение изменено на -1"
+        } else {
+            historyTextView.text += "\n[\(getDate(Date.now))]: Попытка уменьшить значение счетчика ниже 0"
+        }
+    }
+    
+    @IBAction func setToZero() {
+        counter = 0
+        counterLabel.text = "\(counter)"
+        historyTextView.text += "\n[\(getDate(Date.now))]: Значение сброшено"
+    }
 }
-
